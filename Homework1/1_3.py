ccfd455e4b325083febd42ca697ee624f63d5ad5
KeNnY <kenny_0@mail.ru>
2023-04-09 18:26:17 +0300

    Correct the first homework

--- Homework1/1_3.py
+++ Homework1/1_3.py
@@ -14,8 +14,10 @@ def different_of_two_arrays(arr1: list, arr2:list) -> list:
         raise InvalidLengthArrays("The length these two arrays isn't the same")
     diff_arr = []
     for i in range(len(arr1)):
+        if arr1[i] == None or arr2[i] == None:
+            raise TypeError("One or more of the elements in the arrays is None")
         if isinstance(arr1[i], str) or isinstance(arr2[i], str):
-            raise TypeError('One or more of the elements in the array is the string')
+            raise TypeError('One or more of the elements in the arrays is the string')
         if arr2[i] == 0:
             raise ZeroDivisionError("The divider can't be equal 0")
         diff_arr.append(round(arr1[i] / arr2[i], 2))
@@ -49,6 +51,13 @@ if __name__ == "__main__":
     # dif_arr = different_of_two_arrays(arr1=arr1, arr2=arr2)
     # print(dif_arr)
 
+# TypeError with None
+    # arr1= [randint(10, 50) for _ in range(10)]
+    # print(arr1)
+    # arr2= [None,5,3,8,5,2,9,44,2,9]
+    # print(arr2)
+    # dif_arr = different_of_two_arrays(arr1=arr1, arr2=arr2)
+    # print(dif_arr)
 
 
 # without exception
